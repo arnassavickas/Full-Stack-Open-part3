8ec12b19ecfd4e261d16bb9586d9d64e9ffa3c22
Arnas Savickas <arnas.savi@gmail.com>
2020-12-10 19:07:07 +0200

    added build

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('build'))
 app.use(
   morgan(function (tokens, req, res) {
     morgan.token("type", function (req, res) {
