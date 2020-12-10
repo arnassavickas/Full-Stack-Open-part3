5417cfca4bad8bf66950145e60c0c340e885de5f
Arnas Savickas <arnas.savi@gmail.com>
2020-12-10 17:53:24 +0200

    heroku again

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;1
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
